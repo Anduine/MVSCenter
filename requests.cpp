209e0488b4a7fb7161b3d16d00b10894f2ae767c
Anduine <35.artem.lern@gmail.com>
2024-12-07 20:41:11 +0200

    first commit

---
+++ requests.cpp
@@ -0,0 +1,7 @@
+#include "requests.h"
+
+#include <QDate>
+#include <QString>
+#include <vector>
+
+Requests::Requests() {}
---
+++ requests.h
@@ -0,0 +1,32 @@
+#ifndef REQUESTS_H
+#define REQUESTS_H
+
+#include <QDate>
+#include <QString>
+#include <vector>
+
+struct UserRequest{
+    int id;
+    QDate date;
+    QString status;
+    QString client_name;
+    QString client_passportID;
+    int client_phonenumber;
+    int attempt_number;
+};
+
+class Requests
+{
+private:
+    std::vector<UserRequest> request_list;
+public:
+    Requests();
+
+    void heapSort();
+    void heapify(int heap_size, int cur_index);
+
+    void bubbleSort();
+    void selectionSort();
+};
+
+#endif // REQUESTS_H
